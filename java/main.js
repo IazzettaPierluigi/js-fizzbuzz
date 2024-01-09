83b9680dbe3dd4ff1851a19454a5739d47328bc1
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-09 16:08:08 +0100

    trying to do fizzbuzz

--- java/main.js
+++ java/main.js
@@ -7,6 +7,8 @@ for (let i = 1; i <= 100; i++) {
         document.getElementById('output').innerHTML += '<span class="box" id="fizz">Fizz</span>';
     } else if (i % 5 ===0){
         document.getElementById('output').innerHTML += '<span class="box" id="buzz">Buzz</span>'
+    } else if (i % 5 ===0 && i % 3 ===0){
+        document.getElementById('output').innerHTML += '<span class="box" id="fizzbuzz">FizzBuzz</span>'
     } else {
         document.getElementById('output').innerHTML +=`<span class="box">${i}<span>`;
     }
