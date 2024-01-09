51e7e743d4221ed9a1bfd629de32ecc363fe044c
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-09 16:10:34 +0100

    test

--- java/main.js
+++ java/main.js
@@ -7,7 +7,7 @@ for (let i = 1; i <= 100; i++) {
         document.getElementById('output').innerHTML += '<span class="box" id="fizz">Fizz</span>';
     } else if (i % 5 ===0){
         document.getElementById('output').innerHTML += '<span class="box" id="buzz">Buzz</span>'
-    } else if (i % 5 ===0 && i % 3 ===0){
+    } else if (i % 3 ===0 && i % 5 ===0){
         document.getElementById('output').innerHTML += '<span class="box" id="fizzbuzz">FizzBuzz</span>'
     } else {
         document.getElementById('output').innerHTML +=`<span class="box">${i}<span>`;
