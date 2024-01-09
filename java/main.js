fdaafc4edd89bf4f64b998ee5594e36c4588d49f
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-09 16:14:51 +0100

    fizzbuzz done

--- java/main.js
+++ java/main.js
@@ -3,12 +3,12 @@
 // }
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
-        document.getElementById('output').innerHTML += '<span class="box" id="fizz">Fizz</span>';
-    } else if (i % 5 ===0){
-        document.getElementById('output').innerHTML += '<span class="box" id="buzz">Buzz</span>'
-    } else if (i % 3 ===0 && i % 5 ===0){
+    if (i % 3 ===0 && i % 5 ===0) {
         document.getElementById('output').innerHTML += '<span class="box" id="fizzbuzz">FizzBuzz</span>'
+        } else if (i % 5 ===0){
+        document.getElementById('output').innerHTML += '<span class="box" id="buzz">Buzz</span>';
+    } else if (i % 3 ===0){
+        document.getElementById('output').innerHTML += '<span class="box" id="fizz">Fizz</span>';
     } else {
         document.getElementById('output').innerHTML +=`<span class="box">${i}<span>`;
     }
