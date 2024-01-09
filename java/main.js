e673dceac19fb5b70131eca8974dd9e6c62ec762
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-09 16:01:54 +0100

    boxes with colors

--- java/main.js
+++ java/main.js
@@ -1,3 +1,13 @@
+// for (let i = 1; i <= 100; i++) {
+//     document.getElementById('output').innerHTML += `<span>${i}<span>`
+// }
+
 for (let i = 1; i <= 100; i++) {
-    document.getElementById('output').innerHTML += `<p>${i}<p>`
+    if (i % 3 === 0) {
+        document.getElementById('output').innerHTML += '<span class="box" id="fizz">Fizz</span>';
+    } else if (i % 5 ===0){
+        document.getElementById('output').innerHTML += '<span class="box" id="buzz">Buzz</span>'
+    } else {
+        document.getElementById('output').innerHTML +=`<span class="box">${i}<span>`;
+    }
 }
